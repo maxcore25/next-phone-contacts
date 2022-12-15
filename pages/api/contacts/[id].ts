452e16b7305e144c36a12c63cb269b8b4d7cefd8
Max <mkorobov2002@mail.ru>
2022-12-15 08:50:09 +0300

    Contact ID Endpoint Written

---
+++ pages/api/contacts/[id].ts
@@ -0,0 +1,42 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient, Contact } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Contact | Contact[] | null>
+) {
+  if (req.method === 'GET') {
+    const { id } = req.query;
+
+    const contact = await prisma.contact.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+
+    res.status(200).json(contact);
+  } else if (req.method === 'POST') {
+    const contactData = JSON.parse(req.body);
+
+    const savedContact = await prisma.contact.create({ data: contactData });
+
+    res.status(200).json(savedContact);
+  } else if (req.method === 'PATCH') {
+    const contactData = JSON.parse(req.body);
+
+    const savedContact = await prisma.contact.update({
+      data: contactData,
+      where: contactData.id,
+    });
+
+    res.status(200).json(savedContact);
+  } else if (req.method === 'DELETE') {
+    const contactId = JSON.parse(req.body);
+
+    const deletedContact = await prisma.contact.delete({ where: contactId });
+
+    res.status(200).json(deletedContact);
+  }
+}
