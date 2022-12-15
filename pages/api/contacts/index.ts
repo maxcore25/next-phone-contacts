7b4e1eee4c9ee9ad2bb1a53351c94752ee9a215f
Max <mkorobov2002@mail.ru>
2022-12-15 10:43:47 +0300

    First Contact Created

--- pages/api/contacts/index.ts
+++ pages/api/contacts/index.ts
@@ -5,17 +5,21 @@ const prisma = new PrismaClient();
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Contact | Contact[] | null>
+  res: NextApiResponse<Contact | Contact[] | null | { message: string }>
 ) {
   if (req.method === 'GET') {
     const contacts = await prisma.contact.findMany();
 
     res.status(200).json(contacts);
   } else if (req.method === 'POST') {
-    const contactData = JSON.parse(req.body);
+    const contactData = req.body;
 
-    const contacts = await prisma.contact.create({ data: contactData });
+    try {
+      const contacts = await prisma.contact.create({ data: contactData });
 
-    res.status(200);
+      res.status(200).json({ message: 'OK' });
+    } catch (error) {
+      res.status(500).json({ message: error as string });
+    }
   }
 }
