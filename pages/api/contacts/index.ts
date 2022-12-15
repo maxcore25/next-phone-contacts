0c618c806a9091034b7b736f17947625fc3c416d
Max <mkorobov2002@mail.ru>
2022-12-15 08:51:36 +0300

    Contacts Endpoint Refactored

--- pages/api/contacts/index.ts
+++ pages/api/contacts/index.ts
@@ -11,26 +11,5 @@ export default async function handler(
     const contacts = await prisma.contact.findMany();
 
     res.status(200).json(contacts);
-  } else if (req.method === 'POST') {
-    const contactData = JSON.parse(req.body);
-
-    const savedContact = await prisma.contact.create({ data: contactData });
-
-    res.status(200).json(savedContact);
-  } else if (req.method === 'PATCH') {
-    const contactData = JSON.parse(req.body);
-
-    const savedContact = await prisma.contact.update({
-      data: contactData,
-      where: contactData.id,
-    });
-
-    res.status(200).json(savedContact);
-  } else if (req.method === 'DELETE') {
-    const contactId = JSON.parse(req.body);
-
-    const deletedContact = await prisma.contact.delete({ where: contactId });
-
-    res.status(200).json(deletedContact);
   }
 }
