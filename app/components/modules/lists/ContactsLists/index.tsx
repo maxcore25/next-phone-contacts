ad66da40111906ffb6f7d4bc3311783e1bc48b05
Max <mkorobov2002@mail.ru>
2022-12-15 10:37:37 +0300

    Fetch Contacts List Written

--- app/components/modules/lists/ContactsLists/index.tsx
+++ app/components/modules/lists/ContactsLists/index.tsx
@@ -1,18 +1,33 @@
+import { useState } from 'react';
 import { Button, Stack, Paper, Typography } from '@mui/material';
+import { useQuery } from '@tanstack/react-query';
+import { getContacts } from '@/api/contactsApi';
+import { Contact } from '@prisma/client';
 
 const ContactsLists = () => {
+  const [newTodo, setNewTodo] = useState('');
+
+  const {
+    isLoading,
+    isError,
+    error,
+    data: contacts,
+  } = useQuery<Contact[]>({ queryKey: ['contacts'], queryFn: getContacts });
+
   return (
     <Stack sx={{ gap: '32px' }}>
       <Typography variant='h4' component='h2' sx={{ fontWeight: 'bold' }}>
         Contacts
       </Typography>
       <Stack sx={{ gap: '32px' }}>
-        <Paper elevation={3} sx={{ padding: '16px' }}>
-          John Doe
-        </Paper>
-        <Paper elevation={3} sx={{ padding: '16px' }}>
-          Jack Rose
-        </Paper>
+        {contacts?.map(contact => (
+          <Paper key={contact.id} elevation={3} sx={{ padding: '16px' }}>
+            <Typography variant='h6' component='h6' sx={{ fontWeight: 'bold' }}>
+              {contact.email}
+            </Typography>
+            <Typography>{contact.email}</Typography>
+          </Paper>
+        ))}
       </Stack>
       <Button variant='contained' sx={{ width: 'fit-content' }}>
         Add contact
