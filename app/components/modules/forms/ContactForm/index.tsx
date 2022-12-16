9acda06b5307451f761053f25065ee2d722b074c
Max <mkorobov2002@mail.ru>
2022-12-16 16:23:02 +0300

    Selected Contact Global State Written

--- app/components/modules/forms/ContactForm/index.tsx
+++ app/components/modules/forms/ContactForm/index.tsx
@@ -1,4 +1,6 @@
+import { contactStore } from '@/store';
 import { Button, Stack, TextField, Typography } from '@mui/material';
+import { observer } from 'mobx-react-lite';
 
 const ContactForm = () => {
   return (
@@ -8,10 +10,30 @@ const ContactForm = () => {
       </Typography>
       <Stack component='form' sx={{ gap: '32px' }}>
         <Stack>
-          <TextField variant='outlined' label='Name' margin='normal' />
-          <TextField variant='outlined' label='Email' margin='normal' />
-          <TextField variant='outlined' label='Mobile phone' margin='normal' />
-          <TextField variant='outlined' label='Home phone' margin='normal' />
+          <TextField
+            variant='outlined'
+            label='Name'
+            margin='normal'
+            value={contactStore.selectedContact.name}
+          />
+          <TextField
+            variant='outlined'
+            label='Email'
+            margin='normal'
+            value={contactStore.selectedContact.email}
+          />
+          <TextField
+            variant='outlined'
+            label='Mobile phone'
+            margin='normal'
+            value={contactStore.selectedContact.phone_mobile}
+          />
+          <TextField
+            variant='outlined'
+            label='Home phone'
+            margin='normal'
+            value={contactStore.selectedContact.phone_home}
+          />
         </Stack>
         <Stack direction='row' sx={{ gap: '16px' }}>
           <Button variant='contained' color='secondary'>
@@ -26,4 +48,4 @@ const ContactForm = () => {
   );
 };
 
-export default ContactForm;
+export default observer(ContactForm);
