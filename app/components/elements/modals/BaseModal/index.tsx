37bdfe2b488b021456064768d3b03faa132292d2
Max <mkorobov2002@mail.ru>
2022-12-16 20:56:09 +0300

    Create BaseModal

---
+++ app/components/elements/modals/BaseModal/index.tsx
@@ -0,0 +1,38 @@
+import { Modal } from '@mui/material';
+import { Box } from '@mui/system';
+
+interface IProps {
+  children: any;
+  open: boolean;
+  onClose?: (event: {}, reason: 'backdropClick' | 'escapeKeyDown') => void;
+}
+
+const BaseModal = ({ children, open, onClose }: IProps) => {
+  return (
+    <Modal
+      open={open}
+      onClose={onClose}
+      sx={{
+        p: 8,
+      }}>
+      <Box
+        sx={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          width: '100%',
+          maxWidth: '1200px',
+          background: '#fff',
+          borderRadius: '16px',
+          p: 4,
+          maxHeight: '800px',
+          overflow: 'auto',
+        }}>
+        {children}
+      </Box>
+    </Modal>
+  );
+};
+
+export default BaseModal;
