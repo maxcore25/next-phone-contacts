3e7b6469b93aaf4909defc5aa8800371441626de
Max <mkorobov2002@mail.ru>
2022-12-16 21:42:46 +0300

    Add useReducer

--- app/components/elements/modals/AddContactModal/index.tsx
+++ app/components/elements/modals/AddContactModal/index.tsx
@@ -1,53 +1,101 @@
+import { useReducer } from 'react';
 import BaseModal from '../BaseModal';
 import { contactStore } from '@/store';
 import { Button, Stack, TextField } from '@mui/material';
+import { IContact } from '@/types';
+
+type ActionType = 'NAME' | 'EMAIL' | 'PHONE_MOBILE' | 'PHONE_HOME' | 'RESET';
+
+type Action = { type: ActionType; payload?: string };
+
+const initialState: IContact = {
+  name: '',
+  email: '',
+  phone_home: '',
+  phone_mobile: '',
+};
+
+type State = typeof initialState;
+
+const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case 'NAME':
+      return { ...state, name: action.payload as string };
+    case 'EMAIL':
+      return { ...state, email: action.payload as string };
+    case 'PHONE_MOBILE':
+      return { ...state, phone_mobile: action.payload as string };
+    case 'PHONE_HOME':
+      return { ...state, phone_home: action.payload as string };
+    case 'RESET':
+      return initialState;
+    default:
+      return state;
+  }
+};
 
 const AddContactModal = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const handleAddContact = () => {
+    console.log(state);
+  };
+
+  const handleCloseModal = () => {
+    dispatch({ type: 'RESET' });
+  };
+
   return (
     <BaseModal open={true}>
       <Stack component='form' sx={{ gap: '32px' }}>
         <Stack>
           <TextField
+            type='text'
             variant='outlined'
             label='Name'
             margin='normal'
-            value={contactStore.selectedContact.name}
-            onChange={e => (contactStore.selectedContact.name = e.target.value)}
+            value={state.name}
+            onChange={e => dispatch({ type: 'NAME', payload: e.target.value })}
+            required
           />
           <TextField
+            type='email'
             variant='outlined'
             label='Email'
             margin='normal'
-            value={contactStore.selectedContact.email}
-            onChange={e =>
-              (contactStore.selectedContact.email = e.target.value)
-            }
+            value={state.email}
+            onChange={e => dispatch({ type: 'EMAIL', payload: e.target.value })}
+            required
           />
           <TextField
+            type='text'
             variant='outlined'
             label='Mobile phone'
             margin='normal'
-            value={contactStore.selectedContact.phone_mobile}
+            value={state.phone_mobile}
             onChange={e =>
-              (contactStore.selectedContact.phone_mobile = e.target.value)
+              dispatch({ type: 'PHONE_MOBILE', payload: e.target.value })
             }
+            required
           />
           <TextField
+            type='text'
             variant='outlined'
             label='Home phone'
             margin='normal'
-            value={contactStore.selectedContact.phone_home}
+            value={state.phone_home}
             onChange={e =>
-              (contactStore.selectedContact.phone_home = e.target.value)
+              dispatch({ type: 'PHONE_HOME', payload: e.target.value })
             }
+            required
           />
         </Stack>
-        <Stack direction='row' sx={{ gap: '16px' }}>
-          <Button variant='contained' color='secondary' onClick={() => 1}>
-            Update
+        <Stack direction='row' justifyContent='space-between'>
+          <Button variant='outlined' color='error' onClick={handleCloseModal}>
+            Close
           </Button>
-          <Button variant='outlined' color='error' onClick={() => 1}>
-            Delete
+          <Button variant='contained' onClick={handleAddContact}>
+            Create
           </Button>
         </Stack>
       </Stack>
